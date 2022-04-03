87ae4d81167a24b5ad873a5f7029113eecd13134
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-03 18:40:51 +0530

    commit 2

---
+++ Basic/2_Print.java
@@ -0,0 +1,9 @@
+class Print 
+{
+    public static void main(String args[])
+    {
+        System.out.print("Statement continues without changing the line");
+        System.out.println("Using (println) to change the line or");
+        System.out.print("using '\\n': \n..... ");
+    }
+}
