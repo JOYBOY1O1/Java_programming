f9c811b0f7bf4f13d7b3c6533f3f66dcbc7e4c49
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 22:56:33 +0530

    Update Arithmetic.java

--- Basic/Operators/Arithmetic.java
+++ Basic/Operators/Arithmetic.java
@@ -11,11 +11,11 @@ eg:
 public class Arithmetic {
     public static void main(String[] args) {
         int n1 = 100, n2 = 20;
-        System.out.println("n1 + n2 = " + n1 + n2);
-        System.out.println("n1 - n2 = " + (n1 - n2));
-        System.out.println("n1 * n2 = " + n1 * n2);
-        System.out.println("n1 / n2 = " + n1 / n2);
-        System.out.println("n1 % n2 = " + n1 % n2);
+        System.out.println("n1 + n2 = " +(n1 + n2));
+        System.out.println("n1 - n2 = " +(n1 - n2));
+        System.out.println("n1 * n2 = " +(n1 * n2));
+        System.out.println("n1 / n2 = " +(n1 / n2));
+        System.out.println("n1 % n2 = " +(n1 % n2));
     }
 
 }
