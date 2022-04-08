6e5f5a90f12fd66e433c2091d383272fce775914
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 00:24:52 +0530

    Create Logical.java

---
+++ Basic/Operators/Logical.java
@@ -0,0 +1,16 @@
+package Operators;
+
+/* Logical Operators:
+1. && - Logical and operator - returns true only if both conditions are true
+2. || - Logical or operator - returns true if any one condition is true
+3. ! - Logical not - Reverse the result from true to false and vice versa
+
+*/
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println("Logical Operators:\n"+
+        "1. && - Logical and operator\n"+
+        "2. || - Logical or operator\n"+
+        "3. ! - Logical not operator");
+    }
+}
