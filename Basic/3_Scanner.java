24d980c5e0917cf9977e2672944c5a151a6121e8
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 11:43:27 +0530

    Update 3_Scanner.java

--- Basic/3_Scanner.java
+++ Basic/3_Scanner.java
@@ -1,10 +1,13 @@
 import java.util.*;
 
-class Scanner 
+class Scanner_class
 {
     public static void main(String args[]) 
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String name = sc.next();
+        System.out.println(name);
     }    
 }
