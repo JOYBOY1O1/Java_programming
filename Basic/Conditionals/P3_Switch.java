fa633d56e96dc9f84043c472e5a1582d323eda46
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 16:57:00 +0530

    Update P3_Switch.java

--- Basic/Conditionals/P3_Switch.java
+++ Basic/Conditionals/P3_Switch.java
@@ -1,6 +1,31 @@
 package Conditionals;
 
-public class P3_Switch 
-{
-    
+import java.util.Scanner;
+
+public class P3_Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Choose an option ([1][2][3]):");
+        int age = sc.nextInt();
+
+        switch (age) {
+            case 1:
+                System.out.println("Welcome to Java Programming");
+                break;
+
+            case 2:
+                System.out.println("Beginner level");
+                break;
+
+            case 3:
+                System.out.println("Pro Level");
+                break;
+
+            default:
+                System.out.println("Invalid option");
+
+        }
+
+    }
 }
