f1ab7278726b77b77d193ad3638bc030b2df98ae
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 23:12:22 +0530

    Create Assignment.java

---
+++ Basic/Operators/Assignment.java
@@ -0,0 +1,20 @@
+package Operators;
+
+import java.util.Scanner;
+
+/*
+OPERATORS in Java
+Assignment Operator
+*/
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println("(=) is and assignment operator , which assigns the value to a specific variable");
+
+        int n1 = 45, n2 = 55; // assigns value
+        System.out.println(n1+n2);  
+        n1 += 5;
+        System.out.println(n1);  
+        System.out.println(n1+n2);  
+
+    }
+}
