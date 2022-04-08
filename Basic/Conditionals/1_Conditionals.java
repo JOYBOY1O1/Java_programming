5ad3c8796a8a8ca79474cfe2f24578774a62f33e
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 13:02:19 +0530

    indent

--- Basic/Conditionals/1_Conditionals.java
+++ Basic/Conditionals/1_Conditionals.java
@@ -2,19 +2,15 @@ package Conditionals;
 
 import java.util.*;
 
-class Conditionals 
-{
-    public static void main(String args[]) 
-    {
+class Conditionals {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your age:"); 
+        System.out.print("Enter your age:");
         int age = sc.nextInt();
-        if(age>=18)
-        {
+        if (age >= 18) {
             System.out.println("You are an Adult");
-        }
-        else
-        System.out.println("You are a Minor"); 
-    }    
+        } else
+            System.out.println("You are a Minor");
+    }
 }
