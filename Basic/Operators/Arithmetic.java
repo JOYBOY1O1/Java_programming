c6ffbdd0b73a6ba3a0abefbde3f630d65b26a936
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 22:53:49 +0530

    Create Arithmetic.java

---
+++ Basic/Operators/Arithmetic.java
@@ -0,0 +1,21 @@
+package Operators;
+
+/*
+OPERATORS in Java
+Arithmetic Operators
+(+,-,*,/,%)
+eg:
+    operand operator operand = result
+        4       +       4    =      8
+*/
+public class Arithmetic {
+    public static void main(String[] args) {
+        int n1 = 100, n2 = 20;
+        System.out.println("n1 + n2 = " + n1 + n2);
+        System.out.println("n1 - n2 = " + (n1 - n2));
+        System.out.println("n1 * n2 = " + n1 * n2);
+        System.out.println("n1 / n2 = " + n1 / n2);
+        System.out.println("n1 % n2 = " + n1 % n2);
+    }
+
+}
