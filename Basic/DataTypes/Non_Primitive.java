c991e439dbd3fc6a90ee4a4bd520732e38862641
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 22:27:24 +0530

    Create Non_Primitive.java

---
+++ Basic/DataTypes/Non_Primitive.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+// Non Primitive or Reference Data Type:
+
+public class Non_Primitive {
+    public static void main(String[] args) {
+        System.out.println(
+                "Class, Object, Array, String, and interface are called non-primitive data types in Java. These data types are NOT predefined in Java");
+    }
+}
