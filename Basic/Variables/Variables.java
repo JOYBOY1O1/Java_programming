1f66d96be7ff81fca4fa0b5400a655792a984649
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-08 17:16:24 +0530

    Create Variables.java

---
+++ Basic/Variables/Variables.java
@@ -0,0 +1,21 @@
+package Variables;
+
+public class Variables{
+    // Variables are containers that store data values
+
+    public static void main(String[] args) {
+        String str = "Welcome to Java programming";
+        int n1 = 20;
+        double n2 = 20.22;
+        float n3 = 3.1415926f; // when ever we write a float no. we use f at the end
+        char c = 'z';
+        boolean b = true;
+
+        System.err.println("STRING: "+str);
+        System.err.println("INT:" +n1);
+        System.err.println("DOUBLE: "+n2);
+        System.err.println("FLOAT: "+n3);
+        System.err.println("BOOLEAN: "+b);
+        System.err.println("CHAR: "+c);
+    }
+}
