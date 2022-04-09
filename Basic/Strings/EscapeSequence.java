b6b70c90ebec257d6733a3db00026751781eb1fe
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 12:25:18 +0530

    Create EscapeSequence.java

---
+++ Basic/Strings/EscapeSequence.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("\nTo print double codes in String use \\\"");
+        System.out.println("To print slash in String use \\\\");
+        System.out.println("To print slash in String use \\\\");
+        System.out.println("\\n \nfor the next line");
+        System.out.println("Gives a Tab \t Space \\t ");
+    }
+}
