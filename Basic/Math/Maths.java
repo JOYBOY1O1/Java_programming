f93fd4ab7cae91ef82f239c66382b28526f7f4b1
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 13:23:24 +0530

    Create Maths.java

---
+++ Basic/Math/Maths.java
@@ -0,0 +1,17 @@
+package Math;
+
+// Math inbuilt
+public class Maths {
+    public static void main(String[] args) {
+        int n1=11,n2=20;
+        System.out.println("3 pow 2: "+Math.pow(3,2));
+        System.out.println(Math.max(n1,n2));
+        System.out.println(Math.min(n1,n2));
+        System.out.println("sqrt(81): "+Math.sqrt(81));
+        System.out.println("Math.abs(num) converts to Positive: "+Math.abs(-81));
+        System.out.println("Random num between 0 - 1: "+Math.random());
+        System.out.println("Random num between custom values: "+(4+(5-4)*Math.random()));
+
+    }
+    
+}
