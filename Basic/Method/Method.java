1093612228274df524b84d41f4ac18f7790890bf
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 23:42:50 +0530

    Create Method.java

---
+++ Basic/Method/Method.java
@@ -0,0 +1,12 @@
+package Method;
+
+public class Method {
+
+    static int sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10, 20)); // Method calling
+    }
+}
