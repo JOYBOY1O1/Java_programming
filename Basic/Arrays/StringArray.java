2bb46fa565f4025771eb2a4db65ab058fd950234
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 23:12:27 +0530

    Create StringArray.java

---
+++ Basic/Arrays/StringArray.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String [] cars = { "Tesla", "Maruti", "Suzuki", "Honda", "Ford" };
+        for(String value:cars) {
+            System.out.println(value);
+        }
+    }
+}
