2de67353d8f700ed1ab58d60cd2113fe682f4d74
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 21:07:55 +0530

    Create For.java

---
+++ Basic/Loops/For.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class For {
+    public static void main(String[] args) {
+        /*
+         * For Loop
+         * for(statement 1 ; statemnt 2 ; statement 3)
+         * (initial ; condtiton ; update)
+         */
+
+        for (int i = 0; i < 70; i++) {
+            System.out.println(i);
+        }
+    }
+}
