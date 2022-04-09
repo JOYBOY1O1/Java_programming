5a1973665be5f0ccca5a794739d4823e905450dd
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 22:22:04 +0530

    Create P4_Break_Continue.java

---
+++ Basic/Conditionals/P4_Break_Continue.java
@@ -0,0 +1,15 @@
+package Conditionals;
+
+public class P4_Break_Continue {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            if(i==2){
+                continue;
+            }
+              else {
+                  System.out.println(i);
+                }
+        }
+    }
+    
+}
