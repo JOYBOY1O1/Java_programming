7028ca92902cd27c4ec89f1d8103953806a50427
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 20:58:25 +0530

    Create WhileLoop.java

---
+++ Basic/Loops/WhileLoop.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        /*
+        While Loop
+        while(condition){
+            // This code will keep executing until the condition is true
+        }
+        */
+        while(i<70){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
