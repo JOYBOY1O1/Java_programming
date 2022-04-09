830f062bccdd23d55ccb4b3abcccd5000488570b
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 21:00:41 +0530

    Update DoWhile.java

--- Basic/Loops/DoWhile.java
+++ Basic/Loops/DoWhile.java
@@ -2,7 +2,7 @@ package Loops;
 
 public class DoWhile {
     public static void main(String[] args) {
-        
+        //This code runs atlest once even if it is False.
         int i = 0;
         do {
             System.out.println(i);
