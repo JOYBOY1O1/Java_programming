ddb5d2c6357b42050e8ca9785480307e89206a04
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 22:51:55 +0530

    create Array

---
+++ Basic/Arrays/Arrays.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] marks = { 1, 2, 3, 5 };
+        System.out.println("Length of the Array: " + marks.length);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
