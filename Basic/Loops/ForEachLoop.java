f13a71e274ea91f304ece475ab196dca38a81ea1
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-09 22:55:55 +0530

    Create ForEachLoop.java

---
+++ Basic/Loops/ForEachLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        
+        int[] marks = { 1, 2, 3, 4, 5 };
+        // For each loop
+        for(int value:marks){
+            System.out.println(value);
+        }
+    }
+}
