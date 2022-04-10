9be52ca86cc3ac7a54ea582942bdb9aa66b1ccef
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-10 23:19:18 +0530

    Create ArrayWithoutAllocate.java

---
+++ Basic/Arrays/ArrayWithoutAllocate.java
@@ -0,0 +1,22 @@
+package Arrays;
+/*
+Use String to take input and then convert
+use it using Integer.parseInt().
+Using this you don't have to allocate an oversized array.
+*/
+
+import java.util.Scanner;
+
+public class ArrayWithoutAllocate {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements in String: ");
+        String str = sc.nextLine();
+        String[] arr = str.split("\\s+");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + Integer.parseInt(arr[i]));
+        }
+    }
+}
