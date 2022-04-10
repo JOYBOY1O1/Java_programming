a19b855b10a972c6f0cc8d078ed9b87181ec3dcc
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-10 23:03:29 +0530

    Update BinarySearchIterative.java

--- Basic/SearchingAlgorithms/BinarySearchIterative.java
+++ Basic/SearchingAlgorithms/BinarySearchIterative.java
@@ -3,40 +3,50 @@ package SearchingAlgorithms;
 import java.util.Scanner;
 
 public class BinarySearchIterative {
-    int BSearch(int arr[], int begin, int end, int key) {
-        int mid = begin + (end - begin)/2; //advantage (does not overflow quickly)
 
-        if (arr[mid] == key) {
-            return mid;
+    int binarySearch(int arr[], int size, int key) {
+
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+
+            if (arr[mid] < key)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
         }
-        if (arr[mid] < key) {
-            return BSearch(arr, mid + 1, end, key);
-        }
-        if (arr[mid] > key) {
-            return BSearch(arr, begin, end - 1, key);
-        }
-        if (end < begin) {
-            return -1;
-        }
+
         return -1;
     }
 
     public static void main(String args[]) {
+
         BinarySearchIterative obj = new BinarySearchIterative();
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter size of Array: ");
+        System.out.print("Enter size of array: ");
         int size = sc.nextInt();
-        System.out.println("Enter elements: ");
+        
         int[] arr = new int[size];
+
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
+
         System.out.print("Enter element to be found: ");
         int key = sc.nextInt();
-        int result = obj.BSearch(arr, 0, size - 1, key);
+
+        int result = obj.binarySearch(arr, size, key);
+
         if (result == -1)
             System.out.println("Element not present");
+
         else
             System.out.println("Element found at index " + result);
     }
