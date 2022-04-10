a55479e19d369a16c3b3934bbb905d2d4d78e4cd
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-10 14:32:07 +0530

    Create BinarySearchIterative.java

---
+++ Basic/SearchingAlgorithms/BinarySearchIterative.java
@@ -0,0 +1,43 @@
+package SearchingAlgorithms;
+
+import java.util.Scanner;
+
+public class BinarySearchIterative {
+    int BSearch(int arr[], int begin, int end, int key) {
+        int mid = begin + (end - begin)/2; //advantage (does not overflow quickly)
+
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[mid] < key) {
+            return BSearch(arr, mid + 1, end, key);
+        }
+        if (arr[mid] > key) {
+            return BSearch(arr, begin, end - 1, key);
+        }
+        if (end < begin) {
+            return -1;
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BinarySearchIterative obj = new BinarySearchIterative();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of Array: ");
+        int size = sc.nextInt();
+        System.out.println("Enter elements: ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter element to be found: ");
+        int key = sc.nextInt();
+        int result = obj.BSearch(arr, 0, size - 1, key);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
