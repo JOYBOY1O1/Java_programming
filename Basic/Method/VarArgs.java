cfe9e74e00f1cda38d03921ef32f7b11a82dfa28
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-11 09:42:08 +0530

    Create VarArgs.java

---
+++ Basic/Method/VarArgs.java
@@ -0,0 +1,34 @@
+package Method;
+
+public class VarArgs {
+
+    /*
+     * static int add(int a, int b){
+     * return a+b;
+     * }
+     * 
+     * static int add(int a, int b,int c){
+     * return a+b;
+     * }
+     * 
+     * static int add(int a, int b,int c ,int d){
+     * return a+b;
+     * }
+     * 
+     * Instead of making muliple methods we Use VarArgs(...)variable arguments
+     */
+
+    static int add(int... arr) {
+        int result = 0;
+        for (int a : arr) {
+            result = result + a;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(add(2, 3, 4));
+        System.out.println(add(2, 4, 8, 16));
+    }
+}
