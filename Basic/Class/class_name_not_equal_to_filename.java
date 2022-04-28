6c1f4c5ddfb943d8257d6b48c933a584796809fa
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-28 19:29:16 +0530

    Create class_name_not_equal_to_filename.java

---
+++ Basic/Class/class_name_not_equal_to_filename.java
@@ -0,0 +1,12 @@
+class c1 {
+    public static void main(String[] args) {
+        System.out.println("Running code with Different class and file name.");
+        System.out.println("There will not be not error until the class is not public.");
+    }
+}
+
+class c2 {
+    public static void main(String[] args) {
+        System.out.println("Class 2: file name: rough.java class name c1");
+    }
+}
