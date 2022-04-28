e8ecfdc055ce6e1d63c4e6258dcbcf32bee65f9c
Joy <97195469+JOYBOY1O1@users.noreply.github.com>
2022-04-28 19:29:48 +0530

    commit ignore rough

--- Beginning/Rough.java
+++ Beginning/Rough.java
@@ -1,41 +1,12 @@
-import java.util.*;
-
-public class Rough {
+class c1 {
     public static void main(String[] args) {
-        student obj[] = new student[6];
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 6; i++) {
-            String name = sc.next();
-            int age = sc.nextInt();
-            char section = sc.next().charAt(0);
-            int percentage = sc.nextInt();
-
-            obj[i] = new student(name, age, section, percentage);
-        }
-		sc.close();
-        for (int i = 0; i < 6; i++) {
-            obj[i].display();
-        }
+        System.out.println("Running code with Different class and file name.");
+        System.out.println("There will not be not error until the class is not public.");
     }
 }
 
-class student {
-    String name;
-    int age;
-    char section;
-    int percentage;
-
-    student(String n, int a, char s, int p) {
-        name = n;
-        age = a;
-        section = s;
-        percentage = p;
-    }
-
-    void display() {
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(section);
-        System.out.println(percentage);
+class c2 {
+    public static void main(String[] args) {
+        System.out.println("Class 2: file name: rough.java class name c1");
     }
 }
